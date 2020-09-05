1067f6f4c62c35559745ad0eeddeef848006ace1
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-05 20:14:38 -0300

    chore: add env vars

--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@ const App = require("./App");
 
 const UsersModule = require("./modules/users/UsersModule");
 
+require("dotenv").config();
+
 function main() {
   const app = new App();
   app.addModule(new UsersModule());
