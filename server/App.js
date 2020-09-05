5bbc4cac3a1dd14a6773e8e592ee4ca4363cd502
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-05 20:38:50 -0300

    feat: add database connection configuration

--- server/App.js
+++ server/App.js
@@ -1,15 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 class App {
-  constructor() {
+  constructor(env) {
     this.server = express();
+    this.env = env;
   }
 
   addModule(module) {
     this.server.use(module.getRouter());
   }
 
+  configDatabase() {
+    mongoose.connect(this.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    mongoose.connection.on("error", (error) => {
+      console.error(`Connection error: ${error}`);
+    });
+
+    mongoose.connection.once("open", (error) => {
+      console.log(`Database successfully connected`);
+    });
+  }
+
   start() {
+    this.configDatabase();
+
     this.server.listen(3333, () => {
       console.log("Server running on http://localhost:3333");
     });
--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ const UsersModule = require("./modules/users/UsersModule");
 require("dotenv").config();
 
 function main() {
-  const app = new App();
+  const app = new App(process.env);
   app.addModule(new UsersModule());
   app.start();
 }
