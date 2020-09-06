3ae375bce49cb62869068c898b44a487858e0bb9
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-06 18:13:52 -0300

    refactor: change background

--- src/layouts/AuthLayout/index.js
+++ src/layouts/AuthLayout/index.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
+import { COLOR_SHARK } from "../../styles/colors";
+
 export default styled.div`
   height: 100vh;
   width: 100vw;
   overflow: 0;
-  background: rgba(0, 0, 0, 0.1);
+  background: ${COLOR_SHARK};
   padding: 30px;
   display: flex;
   align-items: center;
