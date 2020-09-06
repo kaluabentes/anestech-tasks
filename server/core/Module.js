97087acf8d2f7f9c34eadb8d973c334e4a71db63
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-06 15:05:15 -0300

    refactor: add middlewares

--- server/core/Module.js
+++ server/core/Module.js
@@ -1,11 +1,19 @@
 const express = require("express");
 
 class Module {
-  constructor() {
+  constructor(options) {
     this.router = express();
+
+    if (options && options.middleware) {
+      this.middleware = options.middleware;
+    }
   }
 
   addRoute(verb, path, method) {
+    if (this.middleware) {
+      this.router.use(path, ...this.middleware);
+    }
+
     this.router[verb](path, method);
   }
 
