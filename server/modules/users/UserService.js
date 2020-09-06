b50796428ead3cf6162b822d72a70e46b7afe24f
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-05 22:52:39 -0300

    feat: create user service

---
+++ server/modules/users/UserService.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const User = require("./User");
+const Joi = require("joi");
+
+class UserService {
+  getAll() {
+    return User.find({});
+  }
+
+  async create(body) {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    });
+
+    try {
+      await schema.validateAsync(body);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+
+    return User.create({
+      name: body.name,
+      email: body.email,
+      password: await bcrypt.hash(body.password, 10),
+    });
+  }
+}
+
+module.exports = new UserService();
