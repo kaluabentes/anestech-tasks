28d0d9bebaad81b4963e4e8392909536bf8d8f18
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-06 19:37:45 -0300

    feat: register form

---
+++ src/components/RegisterForm/index.js
@@ -0,0 +1,75 @@
+import React, { useState, useRef } from "react";
+import PropTypes from "prop-types";
+
+import AuthForm from "../AuthForm";
+import AuthLogo from "../AuthLogo";
+import AuthTitle from "../AuthTitle";
+import Input from "../Input";
+import Button from "../Button";
+import { BackButton, BackIcon } from "./styles";
+import logo from "../../images/logo.svg";
+import { useHistory } from "react-router-dom";
+
+const propTypes = {
+  error: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+export default function RegisterForm({ onSubmit, isLoading }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const formRef = useRef();
+  const history = useHistory();
+
+  function handleSubmit() {
+    const formElement = formRef.current;
+
+    if (!formElement.checkValidity()) {
+      formElement.reportValidity();
+      return;
+    }
+
+    onSubmit({ name, email, password });
+  }
+
+  return (
+    <AuthForm ref={formRef}>
+      <BackButton type="button" onClick={() => history.push("/login")}>
+        <BackIcon>arrow_back</BackIcon>
+      </BackButton>
+      <AuthLogo src={logo} alt="React Tasks" />
+      <AuthTitle>Register</AuthTitle>
+      <Input
+        placeholder="Name"
+        type="text"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        required
+        margin="0 0 20px 0"
+      />
+      <Input
+        placeholder="E-mail"
+        type="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        required
+        margin="0 0 20px 0"
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        required
+        margin="0 0 20px 0"
+      />
+      <Button isLoading={isLoading} onClick={handleSubmit}>
+        Registrar
+      </Button>
+    </AuthForm>
+  );
+}
+
+RegisterForm.propTypes = propTypes;
---
+++ src/components/RegisterForm/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import Icon from "../Icon";
+
+export const BackButton = styled.button`
+  padding: 0;
+  display: flex;
+  border: 0;
+  background: transparent;
+  outline: 0;
+  margin-left: -6px;
+`;
+
+export const BackIcon = styled(Icon)`
+  font-size: 30px;
+`;
