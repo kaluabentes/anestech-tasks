9d260993eeae73381f6a597c7785b215819e42c3
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-06 18:04:27 -0300

    feat: implement login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,37 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
+import { useHistory } from "react-router-dom";
 
 import AuthLayout from "../layouts/AuthLayout";
 import LoginForm from "../components/LoginForm";
+import AuthApi from "../services/AuthApi";
+import { useUser } from "../contexts/user";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [, dispatch] = useUser();
+  const history = useHistory();
+
+  async function login(body) {
+    setIsLoading(true);
+
+    try {
+      const { data } = await AuthApi.login(body);
+
+      dispatch({ token: data.token });
+      history.push("/");
+    } catch (error) {
+      setIsLoading(false);
+      alert(error.response.data.message);
+    }
+  }
 
   return (
     <AuthLayout>
       <Helmet>
         <title>Login - React Tasks</title>
       </Helmet>
-      <LoginForm
-        email={email}
-        password={password}
-        isLoading={isLoading}
-        onEmailChange={(event) => setEmail(event.target.value)}
-        onPasswordChange={(event) => setPassword(event.target.value)}
-        onSubmit={() => setIsLoading(true)}
-      />
+      <LoginForm isLoading={isLoading} onSubmit={login} />
     </AuthLayout>
   );
 }
