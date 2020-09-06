6dbd83f42e99f6a2ceed6da1395a6f8394f16c02
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-06 17:08:49 -0300

    feat: auth layout

---
+++ src/layouts/AuthLayout/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default styled.div`
+  height: 100vh;
+  width: 100vw;
+  overflow: 0;
+  background: rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
