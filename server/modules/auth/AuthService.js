7893aae4807ecfa4585b69e84d05d037fea130f3
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-06 19:35:19 -0300

    refactor: add fields

--- server/modules/auth/AuthService.js
+++ server/modules/auth/AuthService.js
@@ -33,6 +33,9 @@ class AuthService {
 
     return {
       token: await jwt.sign(payload, process.env.JWT_SECRET),
+      id: user._id,
+      name: user.name,
+      email: user.email,
     };
   }
 
