227cef09be2d0dd471a75e75e59084b0573d7675
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-07 19:44:48 -0300

    feat: format date hepler

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,4 +1,8 @@
+import padZero from "./padZero";
+
 export default function formatDate(dateString) {
   const date = new Date(dateString);
-  return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} às ${date.getHours()}:${date.getMinutes()}`;
+  return `${padZero(date.getUTCDate())}/${padZero(
+    date.getUTCMonth() + 1
+  )}/${date.getFullYear()} às ${date.getHours()}:${date.getMinutes()}`;
 }
