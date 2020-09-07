1f240001fbefd66e2873ec1baf8f702be125ebdd
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-07 16:51:55 -0300

    feat: add get one and update methods

--- src/services/UsersApi.js
+++ src/services/UsersApi.js
@@ -9,11 +9,19 @@ export default class UsersApi {
     return api.get("/api/v1/users");
   }
 
+  getOne(id) {
+    return api.get(`/api/v1/users/${id}`);
+  }
+
   delete(id) {
     return api.delete(`/api/v1/users/${id}`);
   }
 
   create(body) {
     return api.post("/api/v1/users", body);
+  }
+
+  update(id, body) {
+    return api.post(`/api/v1/users/${id}`, body);
   }
 }
