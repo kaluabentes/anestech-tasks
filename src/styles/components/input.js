84156f025d9edba8187a78c25dbd663f3c7cefb3
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-07 19:44:40 -0300

    feat: create input common styles

---
+++ src/styles/components/input.js
@@ -0,0 +1,24 @@
+import { css } from "styled-components";
+
+import { COLOR_SHARK } from "../../styles/colors";
+
+export default css`
+  font-size: 1rem;
+  padding: 10px 8px;
+  border-radius: 0;
+  width: 100%;
+  outline: none;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+
+  &:focus {
+    border-color: transparent;
+    box-shadow: 0 0 0 2px ${COLOR_SHARK};
+  }
+
+  ${(props) =>
+    props.margin &&
+    css`
+      margin: ${(props) => props.margin}px;
+    `}
+`;
