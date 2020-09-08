859b66d14b9010952389ede0abed3fe3e33c71ab
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-07 22:05:19 -0300

    refactor: add box shadow

--- src/styles/components/card.js
+++ src/styles/components/card.js
@@ -3,5 +3,6 @@ import { css } from "styled-components";
 export default css`
   background: white;
   border-radius: 4px;
-  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
