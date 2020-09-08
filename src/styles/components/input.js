0e572aaf4ca7484ccd62049d34e9d17f2c6733be
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-08 00:31:56 -0300

    fix: adjust input padding

--- src/styles/components/input.js
+++ src/styles/components/input.js
@@ -4,7 +4,7 @@ import { COLOR_SHARK } from "../../styles/colors";
 
 export default css`
   font-size: 1rem;
-  padding: 15px;
+  padding: 0 15px;
   border-radius: 0;
   width: 100%;
   outline: none;
