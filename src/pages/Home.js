b41330978c9603571eb01e1bdcabcb1e0b376da4
Kaluã Bentes <kaluanbentes@gmail.com>
2020-09-07 22:04:55 -0300

    feat: add undone task integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,30 @@ import { Helmet } from "react-helmet";
 
 import AppLayout from "../layouts/AppLayout";
 import PageTitle from "../components/PageTitle";
+import useTasks from "../hooks/useTasks";
+import TaskCard from "../components/TaskCard";
+import formatDate from "../utils/formatDate";
 
 export default function Home() {
+  const tasks = useTasks();
+
   return (
     <AppLayout>
       <Helmet>
         <title>Home - React Tasks</title>
       </Helmet>
       <PageTitle>Em andamento</PageTitle>
+      <TaskCard.Grid>
+        {tasks
+          .filter((task) => !task.endDate)
+          .map((task) => (
+            <TaskCard
+              description={task.description}
+              startDate={`Iniciada em ${formatDate(task.startDate)}`}
+              user={`Por ${task.user.name}`}
+            />
+          ))}
+      </TaskCard.Grid>
     </AppLayout>
   );
 }
